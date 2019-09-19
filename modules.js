435ff21e19b112d14e21ba1cef94a2fe53127ae7
Artem <artemlyksa@gmail.com>
2019-09-19 11:41:51 +0300

    Added usage of OS module

--- modules.js
+++ modules.js
@@ -1,4 +1,15 @@
 //Path module
 const path = require('path');
-let pathObj = path.parse(__filename);
+var pathObj = path.parse(__filename);
+
 console.log(pathObj);
+
+//OS module
+const os = require('os');
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem()
+
+//Template string
+//ES6 / ES2015 : ECMAScript 6
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
